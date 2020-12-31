5655e1c2f374ab1a2ee6725e0fa1b76dd3d324d1
Jan-Philipp Fränken <54367909+janphilippfranken@users.noreply.github.com>
2020-12-31 11:31:24 +0200

    updates

--- client/src/containers/Instruction/InsSummary.js
+++ client/src/containers/Instruction/InsSummary.js
@@ -18,7 +18,7 @@ const InsSummary = props => {
                 2. You will travel to four different villages and decide whether a local space fisherman travelled to the red or blue planet to catch their fish.<br></br><br></br>
                 4. Each fisherman gives you one fish from their catch. Additionally, they give one fish to each of two villagers.<br></br><br></br>
                 5. You will will talk to the two villagers to learn which planet they believe the fisherman travelled.<br></br><br></br>
-                5. The two villagers might talk to each other and influence their beliefs. Thus, you will observe their beliefs multiple times.<br></br><br></br>
+                5. The two villagers might talk to each other and influence their beliefs. Thus, you will observe their beliefs multiple times. Important: You do not know when the two villagers received their fish. Thus, you need to pay attention to both their beliefs and confidence when learning about their relationship.<br></br><br></br>
                 6. You will provide a guess about their relationship and under condieration of their beliefs, select the planet the space fisherman travelled. You will also rate your confidence.<br></br><br></br>
                 
                 </p>
--- client/src/containers/Instruction/InsTask.js
+++ client/src/containers/Instruction/InsTask.js
@@ -20,11 +20,14 @@ const InsTask= props => {
                 
                 <p>Your task is to <b>decide</b> whether the space fisherman travelled the RED or BLUE planet to catch their fish. You will be paid a bonus of 50 cent each time you selected the correct planet that was visited by a village's space fisherman (total bonus = 4 * 50 cent = $2.00).</p><br></br>
 
-                <p>In each village, the local space fisherman gives you <b>one fish</b> from their catch. Additionally, the space fisherman gives one fish to each of <b>two villagers</b>. You <b>do not know</b> which color the fish had.</p><br></br>
+                <p>In each village, the local space fisherman gives you <b>one fish</b> from their catch. Additionally, the space fisherman gives <b>one fish</b> to each of <b>two villagers</b>. You <b>do not know</b> the color of these.</p><br></br>
 
-                <p><b>Before</b> selecting a planet, you will will talk to the two villagers to learn which planet <b>they believe</b> the fisherman travelled.</p><br></br>
+                <p><b>Before</b> selecting a planet, you will will talk to the two villagers to learn which planet <b>they believe</b> the fisherman travelled and how <b>confident</b> they are in their beliefs.</p><br></br>
                 
                 <p><b>Important:</b> The two villagers might <b>talk to each other</b> and <b>influenced</b> their beliefs. Thus, you will observe their beliefs <b>multiple times</b> to find out about their relationships with each other.</p><br></br>
+
+                <p><b>Important:</b> You <b>do not</b> know when the two villagers received their fish. Therefore, pay careful attention to both their <b>beliefs and confidence</b> when finding out about their relationships with each other.</p><br></br>
+               
                 <hr />
                 <img src={fishermanImage} alt="planets"/>
                 <Button clicked={props.goToInstruction}>Next</Button>
