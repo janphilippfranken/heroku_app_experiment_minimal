8ef70c991deab79252ebc2fe5ada3e64a37211c6
Jan-Philipp Fränken <54367909+janphilippfranken@users.noreply.github.com>
2021-01-07 11:28:31 +0200

    updates

--- client/src/containers/Ethics.js
+++ client/src/containers/Ethics.js
@@ -52,10 +52,10 @@ const Ethics = props => {
                 <h2>Information for the participants</h2>
                 <hr />
                 <div className={Classes.ParagraphContainer}>
-                    <p><b>Nature of the study.&nbsp;</b>You are about to participate in an experiment in which you will travel to fictional villages that each received a catch of fish from a space fisherman fishing on either a blue or red planet. For each village, you have to judge whether the space fisherman travelled the blue or red planet to catch their fish. After the experiment, we will ask you you to provide some basic demographics (e.g., age). Your session should last for around 10-15 minutes and your responses will be recorded. You will be given full instructions shortly and will be able to ask any questions you may have.</p>
+                    <p><b>Nature of the study.&nbsp;</b>You are about to participate in an experiment in which you will travel to fictional villages that each received a catch of fish from a space fisherman fishing on either a blue or red planet. For each village, you have to judge whether the space fisherman travelled the blue or red planet to catch their fish. After the experiment, we will ask you you to provide some basic demographics (e.g., age). Your session should last for around 10 minutes and your responses will be recorded. You will be given full instructions shortly and will be able to ask any questions you may have.</p>
                 </div>
                 <div className={Classes.ParagraphContainer}>
-                    <p><b>Compensation.&nbsp;</b> You will be paid £1.50 for your participation + up to $£2.00 bonus based on your performance.</p>
+                    <p><b>Compensation.&nbsp;</b> You will be paid £1.00 for your participation + up to $£2.00 bonus based on your performance.</p>
                 </div>
                 <div className={Classes.ParagraphContainer}>
                     <p><b>Risks and benefits.&nbsp;</b><b>Risks and benefits.&nbsp;</b>There are no known risks to participation in this study. Other than the payment mentioned, there no tangible benefits to you, however you will be contributing to our knowledge about information integration.</p>
